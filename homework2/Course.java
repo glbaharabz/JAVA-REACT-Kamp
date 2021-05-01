50e76699d491f1ea43621783f451861834dc17cf
Gülbahar Abazaoğlu <75485506+glbaharabz@users.noreply.github.com>
2021-05-01 17:06:09 +0300

    Add files via upload

---
+++ homework2/Course.java
@@ -0,0 +1,22 @@
+package homework2;
+
+public class Course {
+
+	public Course () {
+		
+	}
+	public Course(int courseNumber, String courseName, String courseDetail, String instructor ) {
+		this.courseNumber = courseNumber;
+		this.courseName = courseName;
+		this.courseDetail = courseDetail;
+		this.instructor = instructor;
+		
+
+	}
+
+	int courseNumber;
+	String courseName;
+	String courseDetail;
+	String instructor;
+	
+}
