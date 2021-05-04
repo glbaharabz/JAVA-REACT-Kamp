71a4be208f1531ff4cb9ab92fb0350a3672a70fe
Gülbahar Abazaoğlu <75485506+glbaharabz@users.noreply.github.com>
2021-05-04 14:06:03 +0300

    Add files via upload

---
+++ ders kodları/inheritance2/src/inheritance2/CustomerManager.java
@@ -0,0 +1,10 @@
+package inheritance2;
+
+public class CustomerManager {
+	public void add(Logger logger) {
+		// miþteri ekleme kodlarý
+		System.out.println("müþteri eklendi.");
+
+		logger.log();
+	}
+}
---
+++ ders kodları/inheritance2/src/inheritance2/Main.java
@@ -0,0 +1,11 @@
+package inheritance2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		CustomerManager customermanager = new CustomerManager();
+		customermanager.add(new EmailLogger());
+
+	}
+
+}
